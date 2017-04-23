5547e021b1da292afebdd60bdaf7e0ce2365a21a
王安琪 <569450181@qq.com>
2017-04-23 23:15:32 +0800

    Add files via upload

---
+++ app/data.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+
+var username = [{
+    id : 1,
+    name : 'Ystx_MAKEUP',
+    work : 'ins: shengting_x',
+    img :"images/b6.jpg",
+},{
+    id : 2,
+    name : '兔爹哭晕在厕所',
+    work : '微博时尚达人',
+    img :"images/b7.jpg",
+},{
+    id : 3,
+    name : '慧敏_hyemin',
+    work : '韩国欧尼 分享美妆',
+    img :"images/b10.jpg",
+},{
+    id : 4,
+    name : '王小豆沙',
+    work : '微博时尚达人',
+    img :"images/b17.jpg",
+}];
